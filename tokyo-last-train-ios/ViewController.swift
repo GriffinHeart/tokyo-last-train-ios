c9959ef532955a43f7837d9c21fd1f84ab91ca98
Hugo Almeida <squall.griffinheart@gmail.com>
2014-08-04 22:27:02 +0900

    Basic version of the application

--- tokyo-last-train-ios/ViewController.swift
+++ tokyo-last-train-ios/ViewController.swift
@@ -12,8 +12,8 @@ class ViewController: UIViewController {
     
     @IBOutlet var fromTextField: UITextField!
     @IBOutlet var   toTextField: UITextField!
-    
-    @IBOutlet var testTextField: UILabel!
+    @IBOutlet var resultsTextView: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,11 +23,61 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
     @IBAction func fromChanged(sender: AnyObject) {
-        
+        // when writing
     }
     
     @IBAction func toChanged(sender: AnyObject) {
+        // when writing
+    }
+    
+    @IBAction func searchPressed(sender: AnyObject) {
+        // Do search
+        let search = Search(from: fromTextField.text, to: toTextField.text)
+        search.perform()
+        let theRoute = search.lastRoute
+        
+        let time = NSDateFormatter().stringFromDate(theRoute!.getTime())
+        resultsTextView.text = NSString(format: "Line: %@\nStation: %@\nTime: %@", theRoute!.getLine(), theRoute!.getStation(), theRoute!.getTime())
     }
 }
 
+
+
+class Search {
+    var from: String
+    var to: String
+    var lastRoute: LastRoute?
+    
+    init(from: String, to: String){
+        self.from = from
+        self.to = to
+    }
+    
+    func perform() {
+        lastRoute = HyperdiaApi().requestRoute(self.from, to: to)
+    }
+}
+
+class LastRoute {
+    
+    func getStation() -> NSString {
+        return "omotesando"
+    }
+    
+    func getLine() -> String {
+        return "chiyoda-sen"
+    }
+    
+    func getTime() -> NSDate {
+        return NSDate()
+    }
+}
+
+class HyperdiaApi {
+    
+    func requestRoute(from: String, to: String) -> LastRoute {
+        return LastRoute()
+    }
+}
